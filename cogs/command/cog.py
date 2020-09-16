e7094996532612d3d7daade12fd9b500d49ac465
Himmy31 <65179756+Himmy31@users.noreply.github.com>
2020-09-16 19:47:22 +0300

    Update cog.py

--- cogs/command/cog.py
+++ cogs/command/cog.py
@@ -164,6 +164,39 @@ class Cog(commands.Cog):
         conn.commit()
         conn.close()
 
+    @commands.command()
+    async def l(self, ctx, member: discord.Member = None, role: discord.Role = None):
+        conn = sqlite3.connect('voice.db')
+        c = conn.cursor()
+        id = ctx.author.id
+        c.execute("SELECT voiceID FROM voiceChannel WHERE userID = ?", (id,))
+        voice=c.fetchone()
+        if voice is None:
+            embed = discord.Embed(
+                description = f'{ctx.author.mention}, вы не владелец данного канала',
+                color = 0xFF0000)
+            await ctx.channel.send(embed = embed, delete_after = 20)
+        else:
+            if member is None:
+                channelID = voice[0]
+                role = discord.utils.get(ctx.guild.roles, name='@everyone')
+                channel = self.bot.get_channel(channelID)
+                await channel.set_permissions(role, connect=False,read_messages=True)
+                embed = discord.Embed(
+                    description = f'{ctx.author.mention}, закрыл двери в комнату!',
+                    color = 0x2f3136)
+                await ctx.channel.send(embed = embed)
+            else:
+                channelID = voice[0]
+                channel = self.bot.get_channel(channelID)
+                await channel.set_permissions(member, connect = False)
+                embed = discord.Embed(
+                    description = f'{ctx.author.mention}, выгнал {member.mention}',
+                    color = 0x2f3136)
+                await ctx.channel.send(embed = embed)
+
+        conn.commit()
+        conn.close()
 
     @commands.command(aliases = ['открыть'] )
     async def unlock(self, ctx, member: discord.Member = None):
