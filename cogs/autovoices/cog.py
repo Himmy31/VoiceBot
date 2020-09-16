6e4da9d8a39054e20bcd8b2fdb2470e99219eda2
Himmy31 <65179756+Himmy31@users.noreply.github.com>
2020-09-16 17:58:54 +0300

    Update cog.py

--- cogs/autovoices/cog.py
+++ cogs/autovoices/cog.py
@@ -67,7 +67,7 @@ class Cog(commands.Cog):
                     channel2 = await member.guild.create_voice_channel(name,category=category)
                     channelID = channel2.id
                     await member.move_to(channel2)
-                    await channel2.set_permissions(self.bot.user, connect=True,read_messages=True)
+                    await channel2.set_permissions(self.bot.user, connect=True,manage_channels=True)
                     await channel2.edit(name= name, user_limit = limit)
                     c.execute("INSERT INTO voiceChannel VALUES (?, ?)", (id,channelID))
                     conn.commit()
